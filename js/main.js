f738b3c1811b095c3a64e7bca6a663fb5ad53157
welcomegithb <muhammadrizosohibjonov6@gmail.com>
2024-06-19 15:20:49 +0500

    add js masala

---
+++ js/main.js
@@ -0,0 +1,37 @@
+// const age = 19;
+// if (age >= 18) {
+// 	console.log('siz balohat yoshdasiz');
+// }
+
+// let i = 0;
+// do {
+// 	console.log(i);
+// 	i++;
+// 	if (i === 3) {
+// 		console.log(i);
+// 		break;
+// 	}
+// } while (i < 6);
+
+// let i = 0;
+// do {
+// 	i++;
+// 	if (i === 3) {
+// 		continue;
+// 	}
+// 	console.log(i);
+// } while (i < 6);
+
+// for (let i = 1; i <= 100; i++) {
+// 	if (i === 50) {
+// 		break;
+// 	}
+// 	console.log(i);
+// }
+
+for (let i = 1; i <= 100; i++) {
+	if (i === 50) {
+		continue;
+	}
+	console.log(i);
+}
