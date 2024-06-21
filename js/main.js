f99a6029977a534297c3003d66bdfcb224ebe1da
welcomegithb <muhammadrizosohibjonov6@gmail.com>
2024-06-21 13:06:01 +0500

    add js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
+//1
+
+//8
 // const age = 19;
 // if (age >= 18) {
 // 	console.log('siz balohat yoshdasiz');
 // }
-
+//9
 // let i = 0;
 // do {
 // 	console.log(i);
@@ -12,7 +15,7 @@
 // 		break;
 // 	}
 // } while (i < 6);
-
+//10
 // let i = 0;
 // do {
 // 	i++;
@@ -21,17 +24,46 @@
 // 	}
 // 	console.log(i);
 // } while (i < 6);
-
+//11
 // for (let i = 1; i <= 100; i++) {
 // 	if (i === 50) {
 // 		break;
 // 	}
 // 	console.log(i);
 // }
-
-for (let i = 1; i <= 100; i++) {
-	if (i === 50) {
-		continue;
-	}
-	console.log(i);
-}
+//12
+// for (let i = 1; i <= 100; i++) {
+// 	if (i === 50) {
+// 		continue;
+// 	}
+// 	console.log(i);
+// }
+//swich case
+//1
+// let day = 3;
+// let days;
+// switch (day) {
+// 	case 0:
+// 		days = 'Yakshanba';
+// 		break;
+// 	case 1:
+// 		days = 'Dushanba';
+// 		break;
+// 	case 2:
+// 		days = 'Seshanba';
+// 		break;
+// 	case 3:
+// 		days = 'Chorshanba';
+// 		break;
+// 	case 4:
+// 		days = 'Payshanba';
+// 		break;
+// 	case 5:
+// 		days = 'Juma';
+// 		break;
+// 	case 6:
+// 		days = 'Shanba';
+// 		break;
+// 	default:
+// 		days = "Noto'g'ri kun";
+// }
